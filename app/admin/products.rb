3dfe8f21182c650108455e628292735adc388ef8
Baylor Rae <baylor@thecodedeli.com>
2018-03-18 12:28:16 -0500

    use 'categories' association for categories
    Since we are using a `has_many :through` association for products ->
    categories we need to tell ActiveAdmin to use end associtation. For this
    to work we need to also allow ActiveAdmin to receive the `category_ids`
    paramter. Notice how this is the singlar version of the resource
    appended by `_ids`.

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,12 +1,12 @@
 ActiveAdmin.register Product do
 
-permit_params :title, :categories_id, :price, :description, :cover
+    permit_params :title, :price, :description, :cover, :category_ids => []
 
   	form html: { multipart: true } do |f|
 
 	  	f.inputs "Product | New" do
 	    	f.input :title
-	    	f.input :categories_id, as: :select, :collection => Category.all.collect {|category| [category.title, category.id] }
+	    	f.input :categories, as: :select, :collection => Category.all.collect {|category| [category.title, category.id] }
 	    	f.input :price
 	    	f.input :description
 	    	f.input :cover, as: :file
